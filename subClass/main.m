e5f1ae77d1b36ec919e12e7319d1d7e51f141683
sunyanguo <sunyanguo@lvmama.com>
2017-02-21 15:03:39 +0800

    Initial Commit

---
+++ subClass/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  subClass
+//
+//  Created by yanguo sun on 21/02/2017.
+//  Copyright © 2017 Lvmama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
